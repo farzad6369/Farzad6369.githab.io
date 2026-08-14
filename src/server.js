e9858edf99dfabdd01edf4e0b3cc408a5abe8743
farzad6369 <farzad.yazdani63@gmail.com>
2026-08-14 21:44:58 +0330

    Update server.js

--- src/server.js
+++ src/server.js
@@ -1,4 +1,22 @@
+
 import express from 'express';
+import cors from 'cors';
+import dotenv from 'dotenv';
+import { db, refill } from './db.js';
+
+dotenv.config();
+
 const app = express();
-app.get('/', (req, res) => res.send('Server is up!'));
-app.listen(3000, () => console.log('Server running on 3000'));
+app.use(cors());
+app.use(express.json());
+
+app.get('/api/user/:id', (req, res) => {
+  let user = db.prepare('SELECT * FROM users WHERE id = ?').get(req.params.id);
+  if (!user) {
+    db.prepare('INSERT INTO users (id, last_life_at) VALUES (?, ?)').run(req.params.id, new Date().toISOString());
+    user = db.prepare('SELECT * FROM users WHERE id = ?').get(req.params.id);
+  }
+  res.json(refill(user));
+});
+
+app.listen(process.env.PORT || 3000, () => console.log('Server running...'));
